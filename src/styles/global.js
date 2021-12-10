9e9321660c129fa604818cf89b57a80e83ea3e47
henrique.catunda <henriquecatunda2@gmail.com>
2021-12-10 18:37:42 -0300

    style: componente styles global

---
+++ src/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #F8F7FC;
+    font-family: 'Inter', sans-serif;
+  }
+
+  button {
+      margin-right: 11px;
+      border: 0;
+      cursor: pointer;
+      background-color: white;
+  }
+
+  a {
+    color: #242424;
+  }
+
+
+  ::-webkit-input-placeholder {
+      color: #504F52;
+      font-size: 15px;
+    }
+    /* Firefox 19+ */
+  ::-moz-placeholder {
+      color: #504F52;
+      font-size: 15px;
+    
+  }
+
+  .row {
+    margin-left: 15px !important;
+  }
+`
